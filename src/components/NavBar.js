2eef40649cb344851c42a444accc448ce55b9783
Vijay Tiwari <vijay.tiwari.vj7@gmail.com>
2020-11-05 21:58:27 +0530

    add navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Container from 'react-bootstrap/Container';
+
+export const NavBar = () => {
+  return (
+    <Navbar collapseOnSelect expand="lg" bg="light" sticky="top">
+      <Container fluid="md">
+        <Navbar.Brand href="#home">VJ</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  )
+}
+
+export default NavBar;
