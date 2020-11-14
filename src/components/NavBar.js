859559759a0f23da7f328ad21e1dac5ead384111
Vijay Tiwari <vijay.tiwari.vj7@gmail.com>
2020-11-14 16:11:45 +0530

    added icon and logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,9 +5,17 @@ import Container from 'react-bootstrap/Container';
 
 export const NavBar = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" sticky="top" bg="light" variant="light">
+    <Navbar collapseOnSelect expand="lg" sticky="top" bg="light" variant="light" style={{ paddingTop: 3, paddingBottom: 3 }}>
       <Container fluid>
-        <Navbar.Brand href="#home">VJ</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <img
+            src="/logo192.png"
+            width="40"
+            height="40"
+            className="d-inline-block align-top"
+            alt="VJ logo"
+          />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" style={{ border: 'none', color: '#333', padding: 0 }}>
           <i className="fas fa-bars"></i>
         </Navbar.Toggle>
